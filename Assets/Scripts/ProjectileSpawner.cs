6e7896bb14d6cdbc20f1b9244847b8d976e13e2c
Zach-Kolman <zach.kolman@gmail.com>
2024-01-27 21:48:35 -0600

    Projectile now behaves as required

--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -14,6 +14,8 @@ public class ProjectileSpawner : MonoBehaviour
     public AudioClip _deathClip;
     private AudioSource _deathSource;
 
+    private Transform player;
+
     private void Start()
     {
         StartCoroutine(SpawnProjectile());
@@ -21,16 +23,22 @@ public class ProjectileSpawner : MonoBehaviour
         _deathSource = gameObject.AddComponent<AudioSource>();
 
         _deathSource.clip = _deathClip;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     IEnumerator SpawnProjectile()
     {
         yield return new WaitForSeconds(timeBetweenShots);
 
+        spawnPos.LookAt(player);
+
         // Instantiate the projectile with the correct rotation
         GameObject thisProjectile = Instantiate(projectile, spawnPos.position, spawnPos.rotation);
 
         thisProjectile.GetComponent<BasicProjectile>().spawnerPosition = transform;
+
+        print("projectile set to chaser is currently " + thisProjectile.GetComponent<BasicProjectile>().isChaser);
 
         StartCoroutine(SpawnProjectile());
     }
@@ -38,12 +46,12 @@ public class ProjectileSpawner : MonoBehaviour
     IEnumerator DeathRoutine()
     {
         _deathSource.Play();
-        GetComponent<Renderer>().enabled = false;
+        transform.GetChild(0).GetComponent<Renderer>().enabled = false;
         GetComponent<Collider>().enabled = false;
         GetComponent<ProjectileSpawner>().enabled = false;
 
         explosionFX.transform.position = transform.position;
-        explosionFX.GetComponent<ParticleSystem>().Play();
+        explosionFX.GetComponent<ParticleSystem>().Play();  
 
         yield return new WaitForSeconds(_deathClip.length);
 
